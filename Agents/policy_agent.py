375e3267254f9a8116a92b06a7cbdda3a07c9565
sanjitakr <101909282+sanjitakr@users.noreply.github.com>
2025-10-04 20:41:49 +0530

    Update policy_agent.py

--- Agents/policy_agent.py
+++ Agents/policy_agent.py
@@ -1,40 +1,33 @@
-import os
 from openai import OpenAI
+import os
 from dotenv import load_dotenv
-
-# Load API key
 load_dotenv()
+
+
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+def handle_policy_question(question):
+    with open("Knowledge/policy.txt", "r", encoding="utf-8") as f:
+        policy_text = f.read()
 
-
-f = open("policy.txt", "r")
-policy_text = f.read()
-f.close()
-
-
-def ask_policy(question: str):
     prompt = f"""
-    You are a policy assistant for our company HR management software.
-    Answer the question *only* using the following policy text.
-    If the answer is not in the policy, say "I am unable to find this information currently. Please access the company policy"
-    If there is any information regarding confidential information, sensitive information dangerous or top secret then do not display it say"I cannot give this informtion as it is against company guidelines".  POLICY:{policy_text} QUESTION:{question}
+    You are an HR assistant. Use ONLY the following HR policy to answer the question.
+    If the words confidential, secret, do not disclose, or any similar terms used to imply the fact that the data is reserved for for people higher in the company on must not be disclosed via such queries or requires prior permission from authorities then say, "I cannot give this information as it is against the company guidelines"
+
+    POLICY:
+    {policy_text}
+
+    QUESTION:
+    {question}
+
+    If the answer is not found, reply: "I don’t see that in the HR policy. Please go through the policy"
     """
 
-    response = client.chat.completions.create(
+    resp = client.chat.completions.create(
         model="gpt-4o-mini",
-        messages=[{"role": "user", 
-                   "content": prompt}],
+        messages=[{"role": "user", "content": prompt}],
         temperature=0
     )
 
-    answer = response.choices[0].message.content
-    return answer
-
-
-print("Policy Assistant Ready! Type 'exit' to quit.")
-while True:
-    user_q = input("\nWhat questions for you have regarding policy: ")
-    if user_q.lower() in ["exit"]:
-        break
+    return resp.choices[0].message.content
     print("\nAnswer:", ask_policy(user_q))
